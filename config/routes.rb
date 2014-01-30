7f3700452da200e12e0f770fc21a19e8178b5655
Derek Burton <burtond4@nku.edu>
2014-01-30 07:02:59 +0000

    Student controller and some functions written

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 NkuRails::Application.routes.draw do
-  resources :posts do
-    resources :comments
-  end
-  # The priority is based upon order of creation: first created -> highest priority.
+  resources :student
+
+# The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
