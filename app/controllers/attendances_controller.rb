baa9251978f53a0b019ef1ab6d3d4c3615676803
Derek Burton <burtond4@nku.edu>
2014-02-22 01:05:32 +0000

    Attendance working according to Assn 5 specs

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -10,12 +10,24 @@ class AttendancesController < ApplicationController
   
   def create
     @attendance = Attendance.new(attendance_params)
-    if @attendance.save
-      redirect_to attendances_path
-      flash[:created] = "You have sucessfully logged your attendance"
-    else
-      render 'new'
+    attendances = Attendance.where("attended_on = ?", Date.today)
+    bool = true
+    attendances.each do |student|
+      if student.student_identification == @attendance.student_identification
+        bool = false
+      end
     end
+    if bool == true
+      if @attendance.save
+        redirect_to attendances_path
+        flash[:created] = "You have sucessfully logged your attendance"
+      else
+        render 'new'
+      end
+    else
+      flash[:done] = "Already have logged attendance for today"
+      redirect_to attendances_path
+    end  
   end
   
   private
