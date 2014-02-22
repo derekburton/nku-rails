84e09f1f0d9f56f1df57b43624163c6954aa43f0
Derek Burton <burtond4@nku.edu>
2014-02-22 00:36:19 +0000

    Committing files after new clone

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,2 +1,5 @@
 class Attendance < ActiveRecord::Base
+  belongs_to :student
+  validates :seat, numericality: { only_integer: true, message: "Must be an integer" }, 
+    format: { with: /[1-4]/, message: "Place a valid number for seat (1-4)" }
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,19 @@
 class Student < ActiveRecord::Base
   has_secure_password
   validates :password, length: { minimum: 1 }, allow_nil: true
+  
+  has_many :attendances, dependent: :destroy
+  
+  def self.in_seat(seat, date=Date.today)
+    attendances = Attendance.where("attended_on = ? AND seat = ?", date, seat)
+    student_ids = attendances.collect{ |a| a.student_identification }
+    return Student.find(student_ids)
+  end
+
+  def self.absent(date=Date.today)
+    attendances = Attendance.where("attended_on = ?", date)
+    present_students = attendances.collect{ |a| a.student_identification }
+    return Student.where.not(id: present_students)
+  end
+  
 end
